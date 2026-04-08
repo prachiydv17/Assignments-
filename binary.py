8487f2697233b94003683b0204227746794e9466
Prachi <ydvprachi552@gmail.com>
2026-04-08 15:19:00 +0530

    Create binary.py

---
+++ binary.py
@@ -0,0 +1,15 @@
+import pickle as p
+#f=open("prachi1.dat","wb")
+#a=input("enter:")
+#p.dump(a,f)
+#f.close()
+
+#f=open("prachi1.dat","rb")
+#print(p.load(f))
+#f.close()
+
+#with image
+f1="WhatsApp Image 2026-03-28 at 15.47.46.jpeg"
+f2=open("prachi1.dat","wb")
+p.dump(f1,f2)
+f1.close()
